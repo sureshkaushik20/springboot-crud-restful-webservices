481867630c81c02e2a532d4b7774934554264ec2
sureshkaushik20 <sureshkaushik20@gmail.com>
2023-11-27 23:14:05 +0530

    OK

--- src/main/java/net/javaguides/springboot/SpringbootCrudRestfulWebservicesApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootCrudRestfulWebservicesApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootCrudRestfulWebservicesApplication {
 
 	public static void main(String[] args) {
+		System.out.println("OK");
 		SpringApplication.run(SpringbootCrudRestfulWebservicesApplication.class, args);
+
 	}
 
 }
